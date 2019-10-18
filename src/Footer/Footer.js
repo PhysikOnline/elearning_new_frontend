7d25b908594d1210ebbe771d2a3e266702014a2f
Bomel <keiwanjamaly@protonmail.com>
2019-10-18 14:15:59 +0200

    Added weird Annimation Heart to Footer xD

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Footer.css";
 
+import IconFavorite from "../static/IconFavorite";
+
 function Footer() {
   return (
     <footer className="Footer">
-      <div>Made with Heart bei PhysikOnline</div>
+      <div>
+        Made with{" "}
+        <div className="AnnimationSourround">
+          <IconFavorite />
+        </div>{" "}
+        by PhysikOnline
+      </div>
       <div>Impressum Datenschutz</div>
     </footer>
   );
