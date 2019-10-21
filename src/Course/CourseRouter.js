2fd5d1b4ec33027d7a94d9f110ce3be3423b2d9c
Bomel <keiwanjamaly@protonmail.com>
2019-10-21 14:21:56 +0200

    added course Router documentation

--- src/Course/CourseRouter.js
+++ src/Course/CourseRouter.js
@@ -1,18 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import "./CourseRouter.css";
 
 import Course from "./Course";
 
+/**
+ * Create routing to the Course, if the url ends with a cours e.g.
+ *
+ * @version 1.0.1
+ * @author Keiwan Jamaly <keiwan@elearning.physik.uni-frankfurt.de>
+ * @see react-router-dom [Nested Routing](https://reacttraining.com/react-router/web/guides/quick-start/2nd-example-nested-routing)
+ *
+ *
+ * ```
+ * /course/SoSe 19/Experimentalphysik 1
+ * ```
+ *
+ * if it is just
+ *
+ * ```
+ * /course
+ * ```
+ *
+ * it sould display all courses
+ */
 function CourseRouter(props) {
+  // this defines the default path
   let match = useRouteMatch();
   return (
     <div className="CourseRouter">
       <Switch>
+        {/* Route to the Course Page */}
         <Route
           path={`${match.path}/:courseSemester/:courseName`}
           render={path => <Course {...path} login={props.login} />}
         />
+        {/* Route to all Courses Page */}
         <Route path={match.path}>
           <h3>Here should be a overview over all courses</h3>
         </Route>
@@ -20,5 +44,10 @@ function CourseRouter(props) {
     </div>
   );
 }
+CourseRouter.propTypes = {
+  props: PropTypes.shape({
+    login: PropTypes.bool.isRequired
+  })
+};
 
 export default CourseRouter;
