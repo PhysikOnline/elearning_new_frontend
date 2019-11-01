82ee14e63b79e411feb09c61ed9227dc3abc4614
Keiwan Jamaly <keiwanjamaly@gmail.com>
2019-11-01 01:19:29 +0100

    added Group overview to course Router

--- src/Course/Group/GroupRouter.js
+++ src/Course/Group/GroupRouter.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "./GroupRouter.css";
 
 import PropTypes from "prop-types";
 
 import GroupConfig from "./GroupConfig";
+import Loading from "../../Additional/Loading";
 import Group from "./Group";
 
 /**
@@ -16,8 +18,18 @@ class GroupRouter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      groups: []
+      groups: [],
+      assignedGroups: [],
+      openGroup: null,
+      firstRender: true,
+      isLoadingGroups: true,
+      isLoadingAssignedGroups: true
     };
+    this.returnGroupBlock = this.returnGroupBlock.bind(this);
+    this.unsetOpenGroup = this.unsetOpenGroup.bind(this);
+  }
+  unsetOpenGroup() {
+    this.setState({ openGroup: null });
   }
   /**
    * function to fetch all groups
@@ -39,21 +51,133 @@ class GroupRouter extends React.Component {
             JSON.stringify(responseJSON) !==
             JSON.stringify(previousState.groups)
           ) {
-            return { groups: responseJSON };
+            return { groups: responseJSON, isLoadingGroups: false };
           }
         });
       });
+    fetch(
+      "/course/group/assignedgroup?Semester=" +
+        this.props.courseSemester +
+        "&CourseName=" +
+        this.props.courseName,
+      {
+        method: "GET"
+      }
+    )
+      .then(response => response.json())
+      .then(responseJSON => {
+        this.setState(previousState => {
+          if (
+            JSON.stringify(responseJSON) !==
+            JSON.stringify(previousState.assignedGroups)
+          ) {
+            return { assignedGroups: responseJSON };
+          }
+        });
+      })
+      .then(
+        this.setState(previousState => {
+          if (
+            !previousState.openGroup &&
+            previousState.assignedGroups.length === 1 &&
+            previousState.firstRender
+          ) {
+            return {
+              openGroup: previousState.assignedGroups[0],
+              firstRender: false
+            };
+          }
+        })
+      )
+      .then(
+        this.setState(previousState => {
+          if (previousState.isLoadingAssignedGroups)
+            return { isLoadingAssignedGroups: false };
+        })
+      );
   }
   componentDidMount() {
     this.getGroups();
   }
+  componentDidUpdate() {
+    this.getGroups();
+  }
+  returnGroupBlock(group) {
+    let joinButton;
+    if (
+      !this.props.auth.includes("admin") &&
+      !this.props.auth.includes("tutor") &&
+      this.state.assignedGroups.length === 0
+    ) {
+      joinButton = <input type="button" value="Beitreten" />;
+    } else if (
+      !this.props.auth.includes("admin") &&
+      !this.props.auth.includes("tutor") &&
+      this.state.assignedGroups.length === 1 &&
+      this.state.assignedGroups[0] === group.GroupName
+    ) {
+      joinButton = <input type="button" value="Verlassen" />;
+    }
+    return (
+      <div className="GroupBlock" onClick={this.handleGroupClick}>
+        <div className="container">
+          <div className="GroupNameTutor">
+            {this.state.assignedGroups.includes(group.GroupName) ||
+            this.props.auth.includes("admin") ? (
+              <label
+                className="GroupName linkHover"
+                onClick={() => this.setState({ openGroup: group.GroupName })}
+              >
+                {group.GroupName}
+              </label>
+            ) : (
+              <label className="GroupName">{group.GroupName}</label>
+            )}
+            <label className="Tutor">{group.Tutor}</label>
+          </div>
+          <div className="datePicker">
+            <label>{group.Weekday}</label>
+            <label>{group.Starttime}</label>
+            <div>bis</div>
+            <label>{group.Endtime}</label>
+            <label>{group.Room}</label>
+          </div>
+        </div>
+        <div className="buttonContainer">
+          <label>
+            {group.AssignedUser}/{group.Maxuser}
+          </label>
+          {joinButton}
+        </div>
+      </div>
+    );
+  }
+
   render() {
-    // if (this.state.isLoading) {
-    //   return <Loading />;
-    // }
+    if (this.state.isLoadingAssignedGroups || this.state.isLoadingGroups) {
+      return <Loading />;
+    }
     // if (this.state.course.error) {
     //   return <Error error={this.state.course.error} />;
     // }
+
+    if (this.state.openGroup) {
+      return (
+        <div className="GroupRouter">
+          <label className="linkHover" onClick={this.unsetOpenGroup}>
+            Zur Gruppenuebersicht
+          </label>
+          <div>{this.state.openGroup}</div>
+          <Group
+            Group={this.state.groups.find(
+              group => group.GroupName === this.state.openGroup
+            )}
+            auth={this.props.auth}
+          />
+        </div>
+      );
+    }
+
     // define group settings varriable
     let showGroupAdmin;
     // show group settings, if the user is an admin
@@ -74,7 +198,8 @@ class GroupRouter extends React.Component {
         {/* display showGroupAdmin, if it is defined */}
         {showGroupAdmin}
         {this.state.groups.map(x => (
-          <Group Group={x} auth={this.props.auth} key={x.GroupName} />
+          <div key={x.GroupName}>{this.returnGroupBlock(x)}</div>
+          // <Group Group={x} auth={this.props.auth} key={x.GroupName} />
         ))}
       </div>
     );
